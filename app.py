1667e1cf3f4c8dcd8d2e2ca6789d29ff15be41df
surajpandey11 <73161910+surajpandey11@users.noreply.github.com>
2021-05-04 16:05:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ import pickle
 
 app=Flask(__name__)
 
-MODEL_PATH = 'models/model2.h5'
+# MODEL_PATH = "models/model2.h5"
 # model_2_path="models/rfc.pkl"
 sooraj_model1_path="models/2nd_model.h5"
 sooraj_model2_path="models/model_2d_mfcc.h5"
 # Load  trained modelflask 
-model = load_model(MODEL_PATH)
+# model = load_model(MODEL_PATH)
 # model_2=pickle.load(open(model_2_path,'rb'))
 sooraj_model1=load_model(sooraj_model1_path)
 sooraj_model2=load_model(sooraj_model2_path)
